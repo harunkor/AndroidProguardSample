2dae4d8ac53752318bf4553fd0baffdfb2dd97f6
harunkor <harunkor@gmail.com>
2022-06-13 22:32:41 +0300

    Initial commit

---
+++ app/src/main/java/com/harunkor/androidproguardsample/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.harunkor.androidproguardsample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
